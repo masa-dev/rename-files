37a6040cbe929635b4c3154dac219d226dbe8dae
masa-dev <60791106+masa-dev@users.noreply.github.com>
2020-07-07 17:39:55 +0900

    設定をconfig.jsにまとめるなど
    設定をconfig.jsにまとめる．
    js，cssのフォルダをそれぞれ作成．
    インデントの整理
    設定の変数の反映．　

---
+++ js/config.js
@@ -0,0 +1,8 @@
+let config = {
+    sortType: 0,   //ソートの種別[最終更新日，サイズ，タイプ，名前]
+    frontName: '', //番号の前に入る指定された名前
+    startNumber: 0,    //開始番号
+    threeDigitization: false,  // 三桁化
+    noUnderscore: false,    //アンダースコア無し
+    descendingOrder: false  //降順（デフォルトは昇順）
+}
